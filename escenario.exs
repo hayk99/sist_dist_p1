f3e147d77e153a29f88acd3044b5cc0c0ad20a7f
Hayk <hayk@hayk.pc>
2019-10-22 10:33:14 +0200

    spawn en server añadido

--- escenario.exs
+++ escenario.exs
@@ -4,13 +4,13 @@
 defmodule Escenario do
 	def lunchServer (dirs) do
 		Process.register(self(), :server)
-		Node.set_cookie(:cookie)
+		Node.set_cookie(self(), :galleta)
 		IO.puts("Server is up")
 		Server.server()
 	end
 	def lunchClient(server_name, escenario, dir_server, dir_client) do
 		Process.register(self(), :client)
-		Node.set_cookie(:cookie)
+		Node.set_cookie(Node.self(),:galleta)
 		Node.connect(dir_server)
 		Cliente.cliente({server_name, dir_server}, escenario)
 	end
