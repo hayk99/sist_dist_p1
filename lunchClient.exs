20f3f44405afdb2bc7fc91fd35d595232ae36c8d
Hayk <hayk@hayk.pc>
2019-10-22 11:14:03 +0200

    import añadido, pero no va

--- lunchClient.exs
+++ lunchClient.exs
@@ -2,5 +2,6 @@ escenario = :uno
 dir_server = :"server@127.0.0.1"
 dir_client = :"client@127.0.0.1"
 
+import Client
 
 Cliente.lunchClient(:server, escenario, dir_server, dir_client)
--- lunchServer.exs
+++ lunchServer.exs
@@ -2,6 +2,8 @@ escenario = :uno
 dir_server = :"server@127.0.0.1"
 num_workers = 1
 
+import Server
+
 case escenario do 
 	:uno ->		Server.lunchServer(dir_server)
 	:dos ->		Server.lunchServer(dir_server)
